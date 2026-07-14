24b8f1dd4d47979b28b85604c531e534f820c520
DuongLe <vjnagolf@gmail.com>
2026-07-15 01:03:40 +0700

    Update bien_ban.py

--- modules/quan_ly_to/bien_ban.py
+++ modules/quan_ly_to/bien_ban.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+def render_bien_ban(db):
+    st.markdown("### 📝 Biên bản cuộc họp")
+    st.write("Phân hệ này đang được hoàn thiện.")
+    # Thầy có thể thêm form nhập biên bản ở đây sau
