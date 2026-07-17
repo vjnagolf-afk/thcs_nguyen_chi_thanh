f72418a955c9d65f584dea154907d668fe426b6d
DuongLe <vjnagolf@gmail.com>
2026-07-17 08:56:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ if phan_he == "Hỗ trợ Giáo viên":
     st.markdown("## 👩‍🏫 Phân hệ: Hỗ trợ Giáo viên")
     
     # Khai báo đúng số lượng và tên các tab
-    tabs_gv = st.tabs(["XD KHBD", "XD Đề KT", "Thiết kế bài dạy STEM", "Rubric", "Chủ nhiệm", "Quản lý điểm", "Tạo prompt", "Quizizz", "Mô phỏng thực hành"])
+    tabs_gv = st.tabs(["XD KHBD", "XD Đề KT", "Thiết kế bài dạy STEM", "Rubric", "Chủ nhiệm", "Đánh giá kĩ năng viết", "Tạo prompt", "Quizizz", "Mô phỏng thực hành"])
     
     # Gắn liên kết từng hàm vào đúng tab
     with tabs_gv[0]:
