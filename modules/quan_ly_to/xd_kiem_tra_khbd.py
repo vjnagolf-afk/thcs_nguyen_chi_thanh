fe9923532dc848622a534139717c6a9499cd2c75
DuongLe <vjnagolf@gmail.com>
2026-07-18 13:49:33 +0700

    Create xd_kiem_tra_khbd.py

---
+++ modules/quan_ly_to/xd_kiem_tra_khbd.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_kiem_tra_khbd():
+    st.markdown("### 🔎 Kiểm tra, phê duyệt Kế hoạch bài dạy (Giáo án)")
+    st.info("Tính năng rà soát và đánh giá KHBD của giáo viên đang được phát triển...")
