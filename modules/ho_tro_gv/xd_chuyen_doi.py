c2f6e0da0fa4e9936672f64b1d19590ebc3cbf32
DuongLe <vjnagolf@gmail.com>
2026-07-18 11:21:32 +0700

    Create xd_chuyen_doi.py

---
+++ modules/ho_tro_gv/xd_chuyen_doi.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render_chuyen_doi(ai_engine):
+    st.markdown("### 🔄 Chuyển đổi tài liệu thành bài dạy")
+    st.info("Tính năng đang được phát triển...")
+    # Thầy sẽ code logic cho phần này sau
