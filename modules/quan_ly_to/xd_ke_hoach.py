4e49b0fcee7748cf53d6dee07d6983dec7645dc5
DuongLe <vjnagolf@gmail.com>
2026-07-18 13:48:50 +0700

    Create xd_ke_hoach.py

---
+++ modules/quan_ly_to/xd_ke_hoach.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_ke_hoach():
+    st.markdown("### 📅 Xây dựng Kế hoạch Giáo dục của Tổ")
+    st.info("Tính năng lập và theo dõi kế hoạch đang được phát triển...")
