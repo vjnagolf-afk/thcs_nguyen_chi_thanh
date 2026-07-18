b3eefd5cfd93802408280dadbce70009ce30b6e0
DuongLe <vjnagolf@gmail.com>
2026-07-18 13:49:07 +0700

    Create xd_thi_dua.py

---
+++ modules/quan_ly_to/xd_thi_dua.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_thi_dua():
+    st.markdown("### 🏆 Quản lý Thi đua - Khen thưởng")
+    st.info("Tính năng chấm điểm và xếp loại thi đua đang được phát triển...")
