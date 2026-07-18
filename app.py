6e0c63e188309b09e209dab2a563396f60ed3df3
DuongLe <vjnagolf@gmail.com>
2026-07-18 12:30:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ try:
         with tabs[11]: render_tao_hoc_lieu(ai_engine)
     elif phan_he == "Hỗ trợ Giảng dạy":
         st.markdown("## 🪴 Phân hệ: Hỗ trợ Giảng dạy")
-        tabs = st.tabs(["HỎI ĐÁP-RAG", "T.KẾ TRÒ CHƠI", "KIỂM TRA NHANH", "CHẤM BÀI", "TÓM TẮT HỌC LIỆU", "MÔ PHỎNG TN ẢO", " P.TÍCH BÀI HỌC", "P.TÍCH KQHT", "TẠO VIDEO", "TRUY VẤN TÀI LIỆU", "NGÂN HÀNG ĐỀ", "CÁ NHÂN HÓA"])
+        tabs = st.tabs(["HỎI ĐÁP", "TRÒ CHƠI", "K.TRA NHANH", "CHẤM BÀI", "TÓM TẮT HỌC LIỆU", "MÔ PHỎNG TN ẢO", " P.TÍCH BÀI HỌC", "P.TÍCH KQHT", "TẠO VIDEO", "TRUY VẤN T.LIỆU", "TẠO ĐỀ NHANH", "CÁ NHÂN HÓA"])
         with tabs[0]: render_rag(ai_engine)
         with tabs[1]: render_xd_tro_choi(ai_engine)
         with tabs[2]: render_xd_cham_nhanh(ai_engine)
