e4bf6617389e6d0ef0433c7cf805f58903dae990
DuongLe <vjnagolf@gmail.com>
2026-07-20 17:08:27 +0700

    Update xd_de_kt.py

--- modules/ho_tro_gv/xd_de_kt.py
+++ modules/ho_tro_gv/xd_de_kt.py
@@ -196,7 +196,7 @@ def render_xd_de_kt(ai_engine):
             "Lớp 8",
             "Lớp 9",
             "Lớp 10",
-            "Lớp 11"
+            "Lớp 11",
             "Lớp 12"
         ],
         index=2,
