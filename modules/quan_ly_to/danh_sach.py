c1f9058e07a7c3f2381bd5c2adc4854742ec01c0
DuongLe <vjnagolf@gmail.com>
2026-08-05 10:21:06 +0700

    Update danh_sach.py

--- modules/quan_ly_to/danh_sach.py
+++ modules/quan_ly_to/danh_sach.py
@@ -17,13 +17,13 @@ def render_danh_sach():
     # 1. Khai báo danh sách giáo viên
     ds_thanh_vien = [
         {"Họ và tên": "Lê Hồng Dưỡng", "Năm sinh": "1976", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Tổ trưởng", "Email": "vjnagolf@gmail.com", "Điện thoại": "0984331178"},
-        {"Họ và tên": "Nguyễn Thị Huyền Trang", "Năm sinh": "1983", "Bằng cấp": "Thạc sĩ", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123457"},
+        {"Họ và tên": "Nguyễn Thị Huyền Trang", "Năm sinh": "1983", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123457"},
         {"Họ và tên": "Lý Nguyễn Thu Nhi", "Năm sinh": "1979", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123458"},
         {"Họ và tên": "Khương Thị Thúy Vân", "Năm sinh": "1979", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Sinh)", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123460"},
         {"Họ và tên": "Trần Xuân Hạnh", "Năm sinh": "1985", "Bằng cấp": "ĐH", "Chuyên môn": "GDTC", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123461"},
         {"Họ và tên": "Trương Vĩnh Văn", "Năm sinh": "1981", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Sinh) - GDTC", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123462"},
         {"Họ và tên": "Phạm Xuân Thọ", "Năm sinh": "1979", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Sinh) - GDTC", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123463"},
-        {"Họ và tên": "Lê Hùng Cường", "Năm sinh": "1988", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123464"},
+        {"Họ và tên": "Lê Hùng Cường", "Năm sinh": "1988", "Bằng cấp": "Thạc sĩ", "Chuyên môn": "KHTN (Lý) - CN", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123464"},
         {"Họ và tên": "Phạm Thùy Ngoan", "Năm sinh": "1980", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Hóa)", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123465"},
         {"Họ và tên": "Phạm Thị Minh Anh", "Năm sinh": "2002", "Bằng cấp": "ĐH", "Chuyên môn": "KHTN (Hóa-Sinh)", "Nhiệm vụ": "Giáo viên", "Email": "nguyenvana@gmail.com", "Điện thoại": "0909123466"},
     ]    
